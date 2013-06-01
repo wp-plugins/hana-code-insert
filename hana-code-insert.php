05f3fa9d84bfde6632b803d82673508e2b55f783
nurungji <nurungji@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-01 19:46:04 +0000

    Minor update on the description
    git-svn-id: https://plugins.svn.wordpress.org/hana-code-insert/trunk@721431 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- hana-code-insert.php
+++ hana-code-insert.php
@@ -359,21 +359,10 @@ function confirmSubmit()
 </div>
 
     <p>Thank you for using my plugin. - <a href='http://wpmarketing.org/'>HanaDaddy</a></p>
-<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-<input type="hidden" name="cmd" value="_donations">
-<input type="hidden" name="business" value="hanadaddy@gmail.com">
-<input type="hidden" name="item_name" value="HanaDaddy Donation - Thank you!">
-<input type="hidden" name="no_shipping" value="0">
-<input type="hidden" name="no_note" value="1">
-<input type="hidden" name="currency_code" value="USD">
-<input type="hidden" name="tax" value="0">
-<input type="hidden" name="lc" value="US">
 
-<input type="hidden" name="bn" value="PP-DonationsBF">
-<input type="image" src="https://www.paypal.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-<img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1"><br />
-</form>
-</div>
+    <p>
+    <a href="http://wpmarketing.org/donate.php?id=hanacode" target="_new"><img src="https://www.paypal.com/en_US/i/btn/btn_donate_SM.gif" border="0" /></a>
+    </p>
 
 
 <script type="text/javascript" src="http://wpmarketing.org/plugin_news.php?id=<?php echo urlencode($this->tag_name); ?>"></script>
